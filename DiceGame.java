65f2ccb32956467d1a863c7c3f16bd955421cb9b
Ivanhei <lawtakhei@yahoo.com.hk>
2019-07-09 22:20:39 +0900

    2nd version of DiceGame

--- DiceGame.java
+++ DiceGame.java
@@ -23,12 +23,20 @@ public class DiceGame {
 		die2.roll();
 	}
 
+	public int getSum() {
+		sum = die1.getFaceValue() + die2.getFaceValue();
+		return sum;
+	}
+
 	public static void main(String args[]) {
 		System.out.println("Rolling the dice...");
 		DiceGame playGame = new DiceGame();
 		playGame.play();
 		System.out.println("Die 1: " + playGame.die1.getFaceValue());
 		System.out.println("Die 2: " + playGame.die2.getFaceValue());
-		System.out.println("Total value: " + (playGame.die1.getFaceValue()+playGame.die2.getFaceValue()));
+		System.out.println("Total value: " + playGame.getSum());
+
+		if(playGame.getSum() > 7) System.out.println("You won!");
+		else System.out.println("You lost!");
 	}
 }
